127058af849fb0e5105f3739e33c6ece62e63bf4
hprasath <hariprasath_thiagarajan@condenast.com>
2024-11-06 22:15:18 +0530

    adding bad code

--- bad_code.py
+++ bad_code.py
@@ -1,31 +1,45 @@
-def f(x):
-    if x > 0:
-        return True
-    else:
-        return False
+def complex_function(data):
+    result = []
+
+    for i in range(len(data)):
+        if data[i] > 0:
+            for j in range(len(data)):
+                if data[j] % 2 == 0:
+                    for k in range(len(data)):
+                        if data[k] < 10:
+                            for l in range(len(data)):
+                                if data[l] > 5:
+                                    result.append((data[i], data[j], data[k], data[l]))
+                                else:
+                                    result.append((data[i], data[j], data[k], -1))
+                        else:
+                            result.append((data[i], data[j], -1, -1))
+                else:
+                    for m in range(len(data)):
+                        if data[m] < 0:
+                            result.append((data[i], -1, -1, -1))
+                        else:
+                            for n in range(len(data)):
+                                if data[n] == 0:
+                                    result.append((-1, -1, -1, -1))
+                                else:
+                                    result.append((data[i], data[j], data[m], data[n]))
+        else:
+            for p in range(len(data)):
+                if data[p] > 20:
+                    result.append((data[i], -2, -2, -2))
+                else:
+                    for q in range(len(data)):
+                        if data[q] == 1:
+                            result.append((-3, -3, -3, -3))
+                        else:
+                            for r in range(len(data)):
+                                result.append((data[i], data[p], data[q], data[r]))
+
+    return result
 
 
-def do_stuff(a, b):
-    c = 0
-    for i in range(1000000):
-        c += i
-    print("The result is: " + str(c))
-    if f(a):
-        print("A is positive")
-    else:
-        print("A is not positive")
-
-    if f(b):
-        print("B is positive")
-    else:
-        print("B is not positive")
-
-
-def main():
-    do_stuff(5, -10)
-    do_stuff(0, 3)
-    do_stuff(-1, -2)
-
-
-if __name__ == "__main__":
-    main()
+# Example usage
+data_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+output = complex_function(data_list)
+print(output)
